4a612fdaa41de2856a70edb9bc0dccf0600eafb2
iyo-bemoore <imadyoussoufi@gmail.com>
2021-08-07 15:37:21 +0200

    Initial commit

---
+++ src/main/java/bemoore/programming/Maven.java
@@ -0,0 +1,13 @@
+package bemoore.programming;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Maven {
+    private final static Logger log = LoggerFactory.getLogger(Maven.class);
+
+    public static void main(String[] args) {
+        log.info("This is an info message");
+        log.debug("This is a debug message");
+    }
+}
